8c8296cef83190ef0b27257a35b28bdcc97e16b4
minhazulislam3765 <135088587+minhazulislam3765@users.noreply.github.com>
2023-05-31 09:19:22 +0600

    Github.md to Mk.py
    Github.md to Mk.py

---
+++ Mk.py
@@ -0,0 +1,10 @@
+# Mk.py
+███    ███ ██████     ███    ██ ██ ██   ██ ██
+ ████  ████ ██   ██    ████   ██ ██ ██  ██  ██     ██ ████ ██ ██████     ██ ██  ██ ██ █████   ██     ██  ██  ██ ██   ██    ██  ██ ██ ██ ██  ██  ██
+ ██      ██ ██   ██ ██ ██   ████ ██ ██   ██ ██                                                      ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ [✓] AUTHOR    > Niki404-Cyber                     [✓] YOUTUBE   > Mr. NIKI                          [✓] WHATSAPP  > +8801318743765
+ [✓] VERSION   > 1.0.  . 
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━     [1] Random Cloning [bangladesh]
+ [2] Random Cloning [pakistan]                     [3] Random Cloning [nigeria]                      [4] Random Cloning [india]
+ [5] Random Mail Cloning                           [0] Exit                                         
+ [?] Choose :
